ac6f0648969fa16582c1d045de6a429c66480a1e
m-shigemori <yasumasashige790@gmail.com>
2025-02-08 14:30:38 +0900

    service

--- gemini_ros/gemini_llm.py
+++ gemini_ros/gemini_llm.py
@@ -3,6 +3,7 @@ import os
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import String
+from gemini_ros.srv import GeminiRequest
 from google import genai
 
 
@@ -10,38 +11,30 @@ class GeminiLLMNode(Node):
     def __init__(self):
         super().__init__("gemini_llm_node")
 
-        api_key = os.environ.get("GEMINI_API_KEY")
-        self.client = genai.Client(api_key=api_key)
+        self.client = genai.Client(api_key = os.environ.get("GEMINI_API_KEY"))
 
-        self.subscription = self.create_subscription(
-            String, "gemini_llm_request", self.handle_llm_request, 10
+        # サービスを定義
+        self.srv = self.create_service(
+            GeminiRequest, 'gemini_llm_service', self.handle_llm_request
         )
         
-        self.publisher = self.create_publisher(
-            String, "gemini_llm_response", 10
+        self.get_logger().info("Gemini LLM サービスが起動しました")
+
+    def handle_llm_request(self, request, response):
+        self.get_logger().info(f"Received request: {request.request}")
+
+        # Gemini APIへのリクエスト
+        gemini_response = self.client.models.generate_content(
+            model="gemini-2.0-flash",
+            contents=request.request,
+            config={"max_output_tokens": 30}
         )
 
-        self.get_logger().info("Gemini LLM が起動しました")
+        response.response = gemini_response.text.replace('*', '').replace('\n', ' ').strip()
+        self.get_logger().info(f"Sending response: {response.response}")
 
-    def handle_llm_request(self, msg):
-        self.request_callback(msg)
+        return response
 
-    def publish_response(self, msg):
-        self.get_logger().info(msg.data)
-
-        response = self.client.models.generate_content(
-            model = "gemini-2.0-flash",
-            contents = msg.data,
-            config = {
-                # "max_output_tokens": 30,
-            }
-        )
-
-        cleaned_response = response.text.replace('*', '').replace('\n', ' ').strip()
-        response_msg = String()
-        response_msg.data = cleaned_response
-        self.publisher.publish(response_msg)
-        self.get_logger().info(cleaned_response)
 
 def main():
     rclpy.init()
