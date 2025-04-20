325b0193048d5bb0409d30f79383db914c16663e
m-shigemori <yasumasashige790@gmail.com>
2025-04-21 03:05:41 +0900

    [FIX] install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 sudo apt update
-sudo apt install -y portaudio19-dev
-sudo apt install -y pavucontrol
-
-pip3 install sounddevice
-pip3 install -q -U google-genai
+pip install -q -U google-genai
